aac154ac8c85d60f4d70e34db4aa84c7e7fa7e8f
Brandon Fonnemann <branjosef@gmail.com>
2020-10-13 23:36:29 -0500

    Done.

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,2 +1,39 @@
 class CitiesController < ApplicationController
+
+  def index
+    @cities = City.all
+  end
+
+  def show
+    @city = City.find(params[:id])
+    @listings = @city.listings
+  end
+
+  # def new
+  #   @city = City.new
+  # end
+
+  # def edit
+  #   @city = City.find(params[:id])
+  # end
+
+  # def create
+  #   @city = City.new(city_params)
+  #   @city.save
+  #   redirect_to @city 
+  # end
+
+  # def update
+  #   @city = City.find(params[:id])
+  #   @city.update(city_params)
+  #   redirect_to @city 
+  # end
+
+
+  private 
+
+  def city_params
+    params.require(:city).permit(:name)
+  end
+
 end
--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,29 @@
 class ListingsController < ApplicationController
+
+def index
+  @listings = Listing.all
+end
+
+def show
+  @listing = Listing.find(params[:id])
+
+end
+
+# def new 
+#   @listing = Listing.new
+# end
+
+# def create
+#   @listing.new(listing_params)
+#   @listing.save 
+#   redirect_to @listing
+# end
+
+
+private
+def listing_params
+params.require(:listing).permit(:address, :listing_type, :title, :description, :price, :neighborhood_id, :host_id)
+end
+
+
 end
